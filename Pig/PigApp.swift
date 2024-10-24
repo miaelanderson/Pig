8f01baf14662e02191b87c6460f29be496d035ba
Mia <miaelanderson@gmail.com>
2024-10-24 09:24:47 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Mia Anderson on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
