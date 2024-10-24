f919c596528f59a9e5b142998eb33e9bce648c51
Mia <miaelanderson@gmail.com>
2024-10-24 10:01:12 -0500

    Added basic game mechanics

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -29,11 +29,18 @@ struct ContentView: View {
                 CustomText(text: "Turn Score: \(turnScore)")
                 HStack {
                     Button("Roll") {
-                        
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold") {
-                        
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                 }
@@ -42,25 +49,46 @@ struct ContentView: View {
             }
         }
     }
-}
-struct CustomText: View {
-    let text: String
-    var body: some View {
-        Text(text).font(Font.custom("Marker Felt", size: 36))
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+            else {
+                turnScore += randomValue
+            }
+        }
+    }
+    struct CustomText: View {
+        let text: String
+        var body: some View {
+            Text(text).font(Font.custom("Marker Felt", size: 36))
+        }
+    }
+    struct CustomButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .frame(width: 50)
+                .font(Font.custom("Marker Felt", size: 24))
+                .padding()
+                .background(.red).opacity(configuration.isPressed ? 0.0 : 1.0)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
     }
 }
-struct CustomButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .frame(width: 50)
-            .font(Font.custom("Marker Felt", size: 24))
-            .padding()
-            .background(.red).opacity(configuration.isPressed ? 0.0 : 1.0)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
 #Preview {
     ContentView()
 }
